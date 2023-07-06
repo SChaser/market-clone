5bc7cb6b02560e0cc98f75eb1a73830d57ad7aa9
SChaser <82812973+SChaser@users.noreply.github.com>
2023-07-06 14:30:20 +0900

    [이미지 저장 및 불러오기 기능]
    svelte/firebase db/storage 사용한
    당근마켓 클론코딩

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,6 +1,36 @@
 <script>
+  import { onMount } from "svelte";
+  import Footer from "../components/Footer.svelte";
+  import { getDatabase, ref, onValue } from "firebase/database";
+
+  const calcTime = (timestamp) => {
+    //한국시간 UTC+9을 세계시간으로 맞추기
+    const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+    const time = new Date(curTime - timestamp);
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+
+    if (hour > 0) return `${hour}시간 전`;
+    else if (minute > 0) return `${minute}분 전`;
+    else if (second > 0) return `${second}초 전`;
+    else return "방금 전";
+  };
+
   let hour = new Date().getHours();
   let min = new Date().getMinutes();
+
+  $: items = [];
+
+  const db = getDatabase();
+  const itmesRef = ref(db, "items/");
+  //onMount() 안에 넣으면 렌더링 될때마다 호출이 된다.
+  onMount(() => {
+    onValue(itmesRef, (snapshot) => {
+      const data = snapshot.val();
+      items = Object.values(data).reverse();
+    });
+  });
 </script>
 
 <header>
@@ -31,43 +61,26 @@
 <div />
 
 <main>
+  {#each items as item}
+    <div class="item-list">
+      <div class="item-list__img">
+        <img alt={item.title} src={item.imgUrl} />
+      </div>
+      <div class="item-list__info">
+        <div class="item-list__info-title">{item.title}</div>
+        <div class="item-list__info-meta">
+          {item.place}
+          {calcTime(item.insertAt)}
+        </div>
+        <div class="item-list__info-price">{item.price}</div>
+        <div class="item-list__info-description">{item.description}</div>
+      </div>
+    </div>
+  {/each}
+
   <a class="write-btn" href="#/write">+ 글쓰기</a>
 </main>
-<footer>
-  <div class="footer-block">
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/home.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">홈</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/document.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">동네생활</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/location.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">내 근처</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/chat.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">채팅</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/user.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">나의 당근</div>
-    </div>
-  </div>
-</footer>
-
+<Footer location="home" />
 <div class="media-info-msg">화면 사이즈를 줄여주세요</div>
 
 <style>
--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,24 +1,97 @@
-<form id="write-form">
+<script>
+  import { getDatabase, ref, set, push } from "firebase/database";
+  import Footer from "../components/Footer.svelte";
+  import {
+    getStorage,
+    getDownloadURL,
+    ref as refImage,
+    uploadBytes,
+  } from "firebase/storage";
+
+  let title;
+  let price;
+  let description;
+  let place;
+  let files;
+
+  const storage = getStorage();
+  const db = getDatabase();
+
+  // 'file' comes from the Blob or File API
+  // uploadBytes(storageRef, files).then((snapshot) => {
+  //   console.log("Uploaded a blob or file!");
+  // });
+
+  //글쓰기
+  function writeUserData(imgUrl) {
+    push(ref(db, "items/"), {
+      title,
+      price,
+      description,
+      place,
+      insertAt: new Date().getTime(),
+      imgUrl,
+    });
+    alert("글쓰기 완료");
+    window.location.hash = "/";
+  }
+
+  //이미지 업로드
+  const uploadFile = async () => {
+    const file = files[0];
+    const name = file.name;
+    const imgRef = refImage(storage, name);
+    await uploadBytes(imgRef, file);
+    const url = await getDownloadURL(imgRef);
+    return url;
+  };
+
+  //이미지 조회
+  const handleSubmit = async () => {
+    const url = await uploadFile();
+    writeUserData(url);
+  };
+</script>
+
+<form id="write-form" on:submit|preventDefault={handleSubmit}>
   <div>
     <label for="">이미지</label>
-    <input type="file" id="image" name="image" />
+    <input type="file" bind:files id="image" name="image" />
   </div>
   <div>
     <label for="">제목</label>
-    <input type="text" id="title" name="title" />
+    <input type="text" id="title" name="title" bind:value={title} />
   </div>
   <div>
     <label for="">가격</label>
-    <input type="number" id="price" name="price" />
+    <input type="number" id="price" name="price" bind:value={price} />
   </div>
   <div>
     <label for="">설명</label>
-    <input type="text" id="description" name="description" />
+    <input
+      type="text"
+      id="description"
+      name="description"
+      bind:value={description}
+    />
   </div>
   <div>
-    <label for="">장소</label> <input type="text" id="place" name="place" />
+    <label for="">장소</label>
+    <input type="text" id="place" name="place" bind:value={place} />
   </div>
   <div>
-    <button type="submit">제출</button>
+    <button class="write-button" type="submit">제출</button>
   </div>
+  <Footer location="write" />
 </form>
+
+<style>
+  .write-button {
+    padding: 5px 12px 5px 12px;
+    background-color: grey;
+    border-radius: 10px;
+    margin: 10px;
+    color: white;
+    cursor: pointer;
+  }
+</style>
